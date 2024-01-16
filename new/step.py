a711e289c337986c42f75810e3ed4230f2618da5
AyatoTomofuji <seb01090@st.osakafu-u.ac.jp>
2024-01-16 18:10:36 +0900

    Make game able to be executed step by step

---
+++ new/step.py
@@ -0,0 +1,8 @@
+from Scenarios import *
+from kessler_game.src.kesslergame.kessler_game_step import Game_step
+from Controller_neuro import Controller_neuro
+
+game = Game_step(controllers=[Controller_neuro], scenario=accuracy_test_1)
+for i in range(100):
+    score, perf, states = game.run_step([480, 180, False, False, 10])
+    print(score, states)
